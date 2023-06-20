5e5105e2ee09a9e19341e3b90ce7eb0200b9035a
Arguello Harold <Arguello Harold@DESKTOP-D3UF7G0>
2023-06-20 08:08:26 -0600

    Agregar archivos de proyecto.

---
+++ CEcuaciones/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MisClases.ES
+{
+    public class Leer //Clase leer
+    {
+        public static short datoShort()
+        {
+			try
+			{
+				return Int16.Parse(Console.ReadLine());
+			}
+			catch (FormatException)
+			{
+				return Int16.MinValue;//valor mas pequeño
+			}
+        }
+		public static int datoInt()
+		{
+			try
+			{
+                return Int32.Parse(Console.ReadLine());
+            }
+			catch (FormatException)
+			{
+				return Int32.MinValue;
+			}
+		}
+		public static long datoLong()
+		{
+			try
+			{
+				return Int64.Parse(Console.ReadLine());
+			}
+			catch (FormatException)
+			{
+				return Int64.MinValue;
+			}
+		}
+		public static float datoFloat()
+		{
+			try
+			{
+				return Single.Parse(Console.ReadLine());
+			}
+			catch (FormatException)
+			{
+				return Single.MinValue;
+			}
+		}
+		public static double datoDouble()
+		{
+			try
+			{
+				return Double.Parse(Console.ReadLine());
+			}
+			catch (FormatException)
+			{
+				return Double.NaN;
+			}
+		}
+    }
+}
---
+++ CEcuaciones/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEcuaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
